85381fbe5720889c10e935c89e5ad75501a8562e
seidow <seidow@stu.kau.edu.sa>
2025-04-22 12:38:08 +0300

    update project

--- src/assets/Backend/appwriteConfig.js
+++ src/assets/Backend/appwriteConfig.js
@@ -1,7 +1,8 @@
 import { Client, Databases, ID } from "appwrite";
 const projectId = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const endPoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint(endPoint)
     .setProject(projectId);
 
 const databases = new Databases(client);
