6f6a994d371b3d4a44a819479b1c9b08c6cd7964
seidow <seidow@stu.kau.edu.sa>
2025-04-22 12:50:08 +0300

    update Navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar() {
     <nav className="bg-gray-800 p-4 min-w-[330px]">
       <div className="container mx-auto flex justify-between items-center flex-col sm:flex-row">
         <div className='flex items-center space-x-2'>
-        < FcTodoList size={40} />
-        <div className="text-white text-7xl sm:text-4xl p-4 font-bold">Todo List</div>
+        < FcTodoList size={100} className='sm:w-[80px] sm:h-[80px]'    />
+        <div className="text-white text-6xl sm:text-4xl p-4 font-bold">Todo List</div>
         </div>
         <div className="text-white text-xl font-semibold">
           <span className="text-blue-500">Organize</span> your tasks
